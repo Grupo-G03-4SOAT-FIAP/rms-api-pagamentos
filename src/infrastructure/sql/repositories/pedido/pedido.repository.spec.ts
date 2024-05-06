b769d71e4ba88c5da09446ca0c71057734610352
diego <diego@MacBook-Pro-de-diego.local>
2024-05-06 19:40:05 -0300

    testes unitarios pedido repository

--- src/infrastructure/sql/repositories/pedido/pedido.repository.spec.ts
+++ src/infrastructure/sql/repositories/pedido/pedido.repository.spec.ts
@@ -1,84 +1,58 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { PedidoRepository } from './pedido.repository';
-import { pedidoSQLDTOFactoryMock } from 'src/mocks/pedido.mock';
-import { SQLDTOFactory } from '../../factories/sql.dto.factory';
 
 describe('PedidoRepository', () => {
   let pedidoRepository: PedidoRepository;
+  let mockDataServices: any;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        PedidoRepository,
-        {
-          provide: SQLDTOFactory,
-          useValue: pedidoSQLDTOFactoryMock,
-        },
-      ],
-    }).compile();
-
-    pedidoRepository = module.get<PedidoRepository>(PedidoRepository);
+  beforeEach(() => {
+    mockDataServices = {
+      pagamento: {
+        create: jest.fn(),
+      },
+      retorno_mp: {
+        create: jest.fn(),
+      },
+    };
+    pedidoRepository = new PedidoRepository(mockDataServices);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it.skip('deve salvar o QR Code do pedido', async () => {
-    const dataServices = {
-      pagamento: {
-        create: jest
-          .fn()
-          .mockResolvedValue({ id: '1', qr_data: 'QR123', date: new Date() }),
-      },
-    };
-
-    // Substitua o dataServices real pelo mock
-    pedidoRepository['dataServices'] = dataServices as any;
-
-    // Chame o método e verifique se ele retorna o registro criado
-    const idPedido = 'pedido123';
-    const qrData = 'QR123';
+  it('should create a payment record', async () => {
+    const idPedido = '123';
+    const qrData = 'qr_code_data';
     const date = new Date();
-    const registroCriado = await pedidoRepository.registrarQRCode(
+
+    const mockPagamento = { id_pedido: idPedido, qr_data: qrData, date: date };
+    mockDataServices.pagamento.create.mockResolvedValue(mockPagamento);
+
+    const result = await pedidoRepository.registrarQRCode(
       idPedido,
       qrData,
       date,
     );
-    expect(registroCriado).toEqual({
-      id: '1',
-      qr_data: 'QR123',
-      date: expect.any(Date),
-    });
 
-    // Verifique se o método create do dataServices foi chamado com os argumentos corretos
-    expect(dataServices.pagamento.create).toHaveBeenCalledWith({
-      id_pedido: idPedido,
-      qr_data: qrData,
-      date: date,
-    });
+    expect(mockDataServices.pagamento.create).toHaveBeenCalledWith(
+      mockPagamento,
+    );
+    expect(result).toEqual(mockPagamento);
   });
 
-  it.skip('deve salvar a mensagem recebedida através do webhook', async () => {
-    const dataServices = {
-      retorno_mp: {
-        create: jest.fn().mockResolvedValue({ id: '1', topic: 'webhook' }),
-      },
-    };
+  it('should create a webhook message', async () => {
+    const id = '123';
+    const topic = 'webhook_topic';
 
-    // Substitua o dataServices real pelo mock
-    pedidoRepository['dataServices'] = dataServices as any;
+    const mockRetornoMP = { id: id, topic: topic };
+    mockDataServices.retorno_mp.create.mockResolvedValue(mockRetornoMP);
 
-    // Chame o método e verifique se ele retorna a mensagem criada
-    const id = '1';
-    const topic = 'webhook';
-    const mensagemCriada = await pedidoRepository.guardarMsgWebhook(id, topic);
-    expect(mensagemCriada).toEqual({ id: '1', topic: 'webhook' });
+    const result = await pedidoRepository.guardarMsgWebhook(id, topic);
 
-    // Verifique se o método create do dataServices foi chamado com os argumentos corretos
-    expect(dataServices.retorno_mp.create).toHaveBeenCalledWith({
+    expect(mockDataServices.retorno_mp.create).toHaveBeenCalledWith({
       id,
       topic,
     });
+    expect(result).toEqual(mockRetornoMP);
   });
 });
